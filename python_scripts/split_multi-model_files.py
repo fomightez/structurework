bf92684c9aa677b9356d52abe25650f29af1a275
Wayne's Bioinformatics Code Portal is <fomightez@gmail.com>
2014-10-21 12:56:37 -0400

    Update split_multi-model_files.py

--- python_scripts/split_multi-model_files.py
+++ python_scripts/split_multi-model_files.py
@@ -1,1 +1,3 @@
-#
+see https://github.com/fomightez/structurework/blob/master/python_scripts/super_basic_multiple_model_PDB_file_splitter.py
+
+Eventually I will have a few versions for users with various needs.
