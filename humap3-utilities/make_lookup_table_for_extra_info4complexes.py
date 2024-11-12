c3e66b9685d2bb8bfa25b5b99d834c0cd1d0cba8
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2024-11-12 14:53:57 -0500

    add synonyms collecting

--- humap3-utilities/make_lookup_table_for_extra_info4complexes.py
+++ humap3-utilities/make_lookup_table_for_extra_info4complexes.py
@@ -11,24 +11,28 @@ def make_lookup_table_for_extra_info4complexes(df, column_with_identifiers):
     from unipressed import UniprotkbClient
     import time
     for acc in accs:
-        uniprot_record = UniprotkbClient.fetch_one(acc)
-        protein_name = uniprot_record['proteinDescription']['recommendedName']['fullName']['value']
-        '''
-        def safe_get_disease_id(comment):
-            try:
-                return comment['disease']['diseaseId']
-            except KeyError:
-                return comment.get('disease', {}).get('diseaseId', 'Unknown disease ID')
-        
-        disease_info_list = [
-            safe_get_disease_id(comment)
-            for comment in uniprot_record['comments']
-            if comment['commentType'] == 'DISEASE'
-        ]
-        
-        if not disease_info_list:
-            disease_info_list = ['None reported']
-        '''
+        if 'SPECIAL_' in acc: # adds handling for those like `SPECIAL_HGNC12413` and `SPECIAL_HLA-A-related` that are in standardized hu.MAP 2.0 data (This way I can use the same function for both version 2.0 and version 3.0 data)
+            uniprot_record = " "
+            protein_name = f"{acc}_protein"
+        else:
+            uniprot_record = UniprotkbClient.fetch_one(acc)
+            protein_name = uniprot_record['proteinDescription']['recommendedName']['fullName']['value']
+            '''
+            def safe_get_disease_id(comment):
+                try:
+                    return comment['disease']['diseaseId']
+                except KeyError:
+                    return comment.get('disease', {}).get('diseaseId', 'Unknown disease ID')
+            
+            disease_info_list = [
+                safe_get_disease_id(comment)
+                for comment in uniprot_record['comments']
+                if comment['commentType'] == 'DISEASE'
+            ]
+            
+            if not disease_info_list:
+                disease_info_list = ['None reported']
+            '''
         disease_info_list = []
         synonyms_info_list =[]
         if 'comments' in uniprot_record:
@@ -37,6 +41,12 @@ def make_lookup_table_for_extra_info4complexes(df, column_with_identifiers):
                     disease_info = comment.get('disease', {})
                     disease_id = disease_info.get('diseaseId', 'Unknown disease ID')
                     disease_info_list.append(disease_id)
+        if 'genes' in uniprot_record:
+            if uniprot_record['genes']:
+                for i in uniprot_record['genes']:
+                    if 'synonyms' in i:
+                        for s in i['synonyms']:
+                            synonyms_info_list.append(s['value'])
         if not disease_info_list:
             disease_info_list = ['None reported']
         disease_info = '; '.join(disease_info_list[:2])
